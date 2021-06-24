d52cb445e0b8a6b0f83b5c2bc4a32ae078bc780a
code-250 <richmunye@gmail.com>
2021-06-24 15:27:24 +0200

    nav-menu-links for mobile

--- js/main.js
+++ js/main.js
@@ -1,12 +1,56 @@
-const menuLink = document.querySelector(".icons");
-const dropdownItem = document.querySelector(".drop-content");
-const link = document.querySelector("#production");
-const closeIcon = document.querySelector(".close-icon");
-const hamburgerIcon = document.querySelector(".hamburger-menu");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("#hamburger-icon");
+const closeMenu = document.querySelector("#close-icon");
+const mobileLinks = document.querySelector(".mobile");
+const removeHeader = document.querySelector(".header-description");
+const productLinks = document.querySelector("#product");
+const companyLinks = document.querySelector("#company");
+const connectLinks = document.querySelector("#connect");
+const productItems = document.querySelector(".products-link");
+const companyItems = document.querySelector(".company-link");
+const connectItems = document.querySelector(".connect-link");
+const productTitle = document.querySelector(".product-title");
+const companyTitle = document.querySelector(".company-title");
+const connectTitle = document.querySelector(".product-title");
 
-menuLink.addEventListener("click", () => {
-  dropdownItem.classList.toggle("active");
-  closeIcon.classList.toggle("active");
-  hamburgerIcon.classList.toggle("active");
-  link.classList.remove("active");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  closeMenu.classList.toggle("active");
+  mobileLinks.classList.toggle("active");
+  removeHeader.classList.toggle("active");
+});
+
+productLinks.addEventListener("click", () => {
+  mobileLinks.classList.remove("active");
+  productItems.classList.toggle("active");
+
+  console.log("this and you");
+});
+
+productTitle.addEventListener("click", () => {
+  productItems.classList.remove("active");
+  mobileLinks.classList.toggle("active");
+});
+companyLinks.addEventListener("click", () => {
+  mobileLinks.classList.remove("active");
+  companyItems.classList.toggle("active");
+
+  console.log("this and you");
+});
+
+companyTitle.addEventListener("click", () => {
+  companyItems.classList.remove("active");
+  mobileLinks.classList.toggle("active");
+});
+connectLinks.addEventListener("click", () => {
+  mobileLinks.classList.remove("active");
+  connectItems.classList.toggle("active");
+
+  console.log("this and you");
+});
+
+connectTitle.addEventListener("click", () => {
+  connectItems.classList.remove("active");
+  mobileLinks.classList.toggle("active");
 });
